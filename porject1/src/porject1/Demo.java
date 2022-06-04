91226a5cd151e0226a9c6383e6770cffee3b6415
asus <asus@LAPTOP-NSNPOFSN>
2022-06-04 09:14:39 +0530

    first commit

---
+++ porject1/src/porject1/Demo.java
@@ -0,0 +1,15 @@
+package porject1;
+
+public class Demo {
+	int x;
+	public  void main (String[] args) 
+	{
+		Demo myObj=new Demo ();
+		myObj . x=40;
+		System .out.println (myObj.x);
+
+	}
+
+}
+
+
