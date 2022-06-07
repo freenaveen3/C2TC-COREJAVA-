8508ab47ad99614bbce145e6852486ab8751b22f
asus <asus@LAPTOP-NSNPOFSN>
2022-06-07 14:44:39 +0530

    first commit

--- project2/src/project2/company.java
+++ project2/src/project2/company.java
@@ -3,9 +3,9 @@ package project2;
 public class company {
 	String name;
 	public static void main (String[]args) {
-		Comany c = new company ();
+		company c = new company ();
 		company c1 = c; c1.name = "c2tc";
 		c = null; 
-		Systm.out.printin(c.name);
+		System.out.println(c.name);
 	}
 }
